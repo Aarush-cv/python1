a51c3fc70921b2ae9d55093f7e8746345650a7c7
Aarush-cv <aarushchaitanyav@gmail.com>
2025-04-24 17:22:54 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+name=input("Hello i am an AI bot, may i have your name please ")
+mood=input (f"hello{name} how is your mood today? good/bad? ")
+if (mood=="good"):
+    print("Happy to hear that ",name)
+elif(mood=="bad"):
+    print("Im sorry to hear that, dont worry",name,"can i do anything to help? ")
+else:
+    print("hey please tell me how u are feeling?, is everything okay with you",name)
